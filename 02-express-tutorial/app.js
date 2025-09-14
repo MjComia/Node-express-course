17ff9e7a5ffb53c70f7f6d403cc4efa0963a95b9
MjComia <markjoel.comia@tup.edu.ph>
2025-09-14 19:54:20 +0800

    feat: add express routes for authentication and people management
    - Added `auth.js` route for handling user authentication with a welcome message.
    - Created `people.js` route to manage people data with CRUD operations.
    - Updated `package.json` to include `morgan` for logging and fixed a typo in `express` dependency.
    - Introduced new HTML file `index.html` for the navbar layout.
    - Added `browser-app.js` for navbar toggle functionality.
    - Included `logo.svg` for branding in the navbar.
    - Created `styles.css` for styling the navbar and overall layout.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,27 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+
+const people = require('./routes/people');
+const auth = require('./routes/auth');
+
+
+app.use(express.static('./methods-public'));
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(express.json());
+
+app.use('/api/people', people )
+app.use('/login', auth )
+
+
+app.get('/api/people', (req,res)=>{
+    res.status(200).json({success: true , data: people})
+});
+
+
+
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000...');
+});
+//6:30
